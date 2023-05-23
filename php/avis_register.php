e69443a8ec6860f2900ad46977256432a854da03
Thibault Soquette <thibault.soquette.pro@gmail.com>
2023-05-23 23:17:57 +0200

    Edit : Avis register
    Signed-off-by: Thibault Soquette <thibault.soquette.pro@gmail.com>

--- php/avis_register.php
+++ php/avis_register.php
@@ -1,26 +1,49 @@
-<?php
-// Récupérer les données du formulaire
-$avis = $_POST['avis'];
-$note = $_POST['note'];
-$file = "../assets/json/avis.json";
+<!DOCTYPE html>
+<html lang="fr">
 
-// Vérifier si le fichier JSON existe
-if (!file_exists($file)) {
-    // Créer un tableau vide pour stocker les avis
-    $avisExistants = array();
-} else {
-    // Charger les avis existants depuis le fichier JSON
-    $avisExistants = file_get_contents($file);
-    $avisExistants = json_decode($avisExistants, true);
-}
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../../assets/css/style.css" />
+    <link rel="shortcut icon" href="../../assets/svg/st-raph.svg" type="image/x-icon" />
+    <script src="https://kit.fontawesome.com/28b70435c7.js" crossorigin="anonymous"></script>
+    <!-- <script src="assets/js/GPS.js"></script> -->
+    <title>Paris | Jeu</title>
+</head>
 
-// Ajouter le nouvel avis à la liste
-$nouvelAvis = array('avis' => $avis, 'note' => $note);
-$avisExistants[] = $nouvelAvis;
+<body>
+    <main>
+        <?php
+        // Récupérer les données du formulaire
+        $avis = $_POST['avis'];
+        $note = $_POST['note'];
+        $file = "../assets/json/avis.json";
 
-// Enregistrer les avis dans le fichier JSON
-$avisJson = json_encode($avisExistants);
-file_put_contents($file, $avisJson);
+        // Vérifier si le fichier JSON existe
+        if (!file_exists($file)) {
+            // Créer un tableau vide pour stocker les avis
+            $avisExistants = array();
+        } else {
+            // Charger les avis existants depuis le fichier JSON
+            $avisExistants = file_get_contents($file);
+            $avisExistants = json_decode($avisExistants, true);
+        }
 
-echo "Merci d'avoir soumis votre avis !";
-?>
+        // Ajouter le nouvel avis à la liste
+        $nouvelAvis = array('avis' => $avis, 'note' => $note);
+        $avisExistants[] = $nouvelAvis;
+
+        // Enregistrer les avis dans le fichier JSON
+        $avisJson = json_encode($avisExistants);
+        file_put_contents($file, $avisJson);
+
+        echo "Merci d'avoir soumis votre avis !";
+        ?>
+    </main>
+    <footer>
+        <p class="copyright">&copy;Thibault Soquette - Tous droits réservés</p>
+    </footer>
+</body>
+
+</html>
