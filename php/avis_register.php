7b456c122959bca74851a2bb390b4f9f6614192a
Thibault Soquette <thibault.soquette.pro@gmail.com>
2023-05-23 23:20:55 +0200

    Edit : ...
    Signed-off-by: Thibault Soquette <thibault.soquette.pro@gmail.com>

--- php/avis_register.php
+++ php/avis_register.php
@@ -14,32 +14,34 @@
 
 <body>
     <main>
-        <?php
-        // Récupérer les données du formulaire
-        $avis = $_POST['avis'];
-        $note = $_POST['note'];
-        $file = "../assets/json/avis.json";
+        <p>
+            <?php
+            // Récupérer les données du formulaire
+            $avis = $_POST['avis'];
+            $note = $_POST['note'];
+            $file = "../assets/json/avis.json";
 
-        // Vérifier si le fichier JSON existe
-        if (!file_exists($file)) {
-            // Créer un tableau vide pour stocker les avis
-            $avisExistants = array();
-        } else {
-            // Charger les avis existants depuis le fichier JSON
-            $avisExistants = file_get_contents($file);
-            $avisExistants = json_decode($avisExistants, true);
-        }
+            // Vérifier si le fichier JSON existe
+            if (!file_exists($file)) {
+                // Créer un tableau vide pour stocker les avis
+                $avisExistants = array();
+            } else {
+                // Charger les avis existants depuis le fichier JSON
+                $avisExistants = file_get_contents($file);
+                $avisExistants = json_decode($avisExistants, true);
+            }
 
-        // Ajouter le nouvel avis à la liste
-        $nouvelAvis = array('avis' => $avis, 'note' => $note);
-        $avisExistants[] = $nouvelAvis;
+            // Ajouter le nouvel avis à la liste
+            $nouvelAvis = array('avis' => $avis, 'note' => $note);
+            $avisExistants[] = $nouvelAvis;
 
-        // Enregistrer les avis dans le fichier JSON
-        $avisJson = json_encode($avisExistants);
-        file_put_contents($file, $avisJson);
+            // Enregistrer les avis dans le fichier JSON
+            $avisJson = json_encode($avisExistants);
+            file_put_contents($file, $avisJson);
 
-        echo "Merci d'avoir soumis votre avis !";
-        ?>
+            echo "Merci d'avoir soumis votre avis !";
+            ?>
+        </p>
     </main>
     <footer>
         <p class="copyright">&copy;Thibault Soquette - Tous droits réservés</p>
